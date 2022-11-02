994c3ee89f8dfbbfef48bef32b567f2cc9a29939
aoiwebstudio <aoiwebstudio@gmail.com>
2022-11-03 07:08:18 +0900

    11/3

--- js/script.js
+++ js/script.js
@@ -27,12 +27,56 @@ new WOW().init();
 
 
 //jQuery
-jQuery('.drawerIcon').on('click', function(e) {
-  e.preventDefault();
+jQuery(function () {
 
-  jQuery('.drawerIcon').toggleClass('is-active');
-  jQuery('.drawerContent').toggleClass('is-active');
-  jQuery('.drawerBackground').toggleClass('is-active');
+  //スムーススクロール
+  jQuery('a[href^="#"]').click(function() {
+    
+    let speed = 300;
+    let id = jQuery(this).attr("href");
+    let target = jQuery("#" == id ? "html" : id);
+    let position = jQuery(target).offset().top;
+    
+    jQuery("html,body").animate(
+      {
+        scrollTop: position - jQuery('#js-header').outerHeight()
+      },
+      speed
+    );
+      
+    return false;  
+  });
 
-  return false;
-});
+  //drawer
+  jQuery('.drawerIcon').on('click', function (e) {
+    e.preventDefault();
+
+    jQuery('.drawerIcon').toggleClass('is-active');
+    jQuery('.drawerContent').toggleClass('is-active');
+    jQuery('.drawerBackground').toggleClass('is-active');
+
+    return false;
+  });
+
+  //formの入力確認
+  let $submit = $('#js-submit');
+  $('#js-form input, #js-form textarea').on('change', function() {
+    if(
+      $('#js-form input[type="text"]').val() !== "" &&
+      $('#js-form input[type="email"]').val() !== "" &&
+      $('#js-form input[type="tel"]').val() !== "" &&
+      $('#js-form textarea').val() !== "" &&
+      $('#js-form input[name="entry.54629690"]').prop('checked') === true,
+      $('#js-form input[type="checkbox"]').prop('checked') === true
+    ) {
+      // すべて入力されたとき
+      $submit.prop('disabled', false);
+      $submit.addClass('-active');
+    } else {
+      // 入力されていないとき
+      $submit.prop('disabled', true);
+      $submit.removeClass('-active');
+    }
+  });
+
+});//jQuery-end
